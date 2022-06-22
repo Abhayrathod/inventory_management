863a2b99d4e0f19042a3c5ab7ef0ec528bc0f62a
Abhay <rathod.abh@gmail.com>
2022-06-22 16:21:48 +0530

    initial commit

---
+++ user/views.py
@@ -0,0 +1,53 @@
+from email import message_from_binary_file
+from django.conf import settings
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib import messages
+from django.core.mail import send_mail
+from config import settings
+
+# Create your views here.
+
+def register(request):
+    if request.method=='POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request,f'Account has been created for {username}. Continue to Log in.')
+            return redirect('user-login')
+    else:
+        form = CreateUserForm()
+    context = {
+        'form': form    
+    }
+    return render (request, 'user/register.html',context)
+
+def profile(request):
+    return render(request, 'user/profile.html')
+
+def profile_update(request):
+    if request.method=='POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return redirect('user-profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+    context={
+        'user_form': user_form,
+        'profile_form': profile_form,
+    }
+    return render(request,'user/profile_update.html',context)
+
+# def send_email(request):
+#     subject = "this is the test email"
+#     message = "hi this is a test email"
+#     email_from = settings.EMAIL_HOST_USER
+#     recipient_list = ['rathod.abh@gmail.com',]
+#     send_mail(subject,message,email_from,recipient_list)
+#     return render(request,"") 
